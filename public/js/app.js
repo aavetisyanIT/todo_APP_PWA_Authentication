76e85c13898538f4d4de18f6c37e4d133b81efe2
aavetisyanit <aavetisyanit@gmail.com>
2021-01-17 08:40:54 -0600

    Add login.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('/public/js/sw.js')
+		.register('/public/sw.js')
 		.then(reg => console.log('service worker is registered', reg))
 		.catch(err => console.log('service worker is not registered', err));
 }
