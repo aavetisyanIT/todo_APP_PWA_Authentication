32a306e2e3e015a12b28f01d4a50c4ccc572a773
aavetisyanit <aavetisyanit@gmail.com>
2021-01-22 22:21:49 -0600

    Sign up users with email and password

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 // When the user clicks anywhere outside of the modals, close it
 window.onclick = function (event) {
-	const modalLogin = document.getElementById('id01');
-	const modalRegister = document.getElementById('id02');
+	const modalLogin = document.querySelector('#id01');
+	const modalRegister = document.querySelector('#id02');
 	const body = document.querySelector('body');
 	if (event.target.lastChild == body) {
 		modalLogin.style.display = 'none';
@@ -16,4 +16,12 @@ signupForm.addEventListener('submit', evt => {
 	const password = signupForm['signup-password'].value;
 
 	//sign up the user
+	auth.createUserWithEmailAndPassword(email, password)
+		.then(cred => {
+			console.log(cred);
+		})
+		.catch(err => console.log(err.message));
+	const modalRegister = document.querySelector('#id02');
+	modalRegister.style.display = 'none';
+	signupForm.reset();
 });
